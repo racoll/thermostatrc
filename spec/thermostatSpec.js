80c6c2344342a9146547c196e07003d641aff810
robincollins <racollins1@me.com>
2018-01-16 22:25:51 +0000

    created up function

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -9,7 +9,12 @@ describe("Thermostat", function () {
   });
 
   it("starts at 20 degrees", function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it("increases temperature with up()", function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
 
 });
