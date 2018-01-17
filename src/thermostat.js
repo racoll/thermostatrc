857f33ffee7fc321578ff0638cb49957675bc617
robin collins <racollins1@me.com>
2018-01-17 17:07:18 +0000

    added max limit with PSM on and max limit with PSM off

--- src/thermostat.js
+++ src/thermostat.js
@@ -4,6 +4,8 @@ function Thermostat() {
   this.temperature = 20;
   this.MINIMUM_TEMPERATURE = 10;
   this.powerSavingMode = true;
+  this.MAX_LIMIT_PSM_ON = 25;
+  this.MAX_LIMIT_PSM_OFF = 32;
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
@@ -11,6 +13,9 @@ Thermostat.prototype.getCurrentTemperature = function() {
 };
 
 Thermostat.prototype.up = function() {
+  if (this.isMaximumTemperature()) {
+    return;
+  }
   this.temperature += 1;
 };
 
@@ -24,6 +29,13 @@ Thermostat.prototype.down = function() {
 Thermostat.prototype.isMinimumTemperature = function() {
   return this.temperature === this.MINIMUM_TEMPERATURE;
 };
+
+Thermostat.prototype.isMaximumTemperature = function() {
+  if (this.isPowerSavingModeOn() === false) {
+    return this.temperature === this.MAX_LIMIT_PSM_OFF;
+  }
+  return this.temperature === this.MAX_LIMIT_PSM_ON;
+}
 
 Thermostat.prototype.isPowerSavingModeOn = function() {
   return this.powerSavingMode === true;
